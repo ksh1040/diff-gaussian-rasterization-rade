db80149d215707eea765041c8b6c704871048d68
ksh1040 <ksh1040@kaist.ac.kr>
2025-01-23 20:49:30 +0900

    Change package name

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name="diff_gaussian_rasterization_rade",
-    packages=['diff_gaussian_rasterization'],
+    packages=['diff_gaussian_rasterization_rade'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="diff_gaussian_rasterization_rade._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
